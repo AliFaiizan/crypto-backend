c2c07890d0f062b10726e60dd7f43debf568a0cb
AliFaiizan <alifaizanahmad@gmail.com>
2022-07-19 13:16:55 -0700

    basic auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+
+@Controller("auth")
+export class AuthController{
+
+    constructor(private  authService: AuthService){}
+
+    @Post('signUp')
+    signUp(){
+        this.authService.signUp()
+    }
+    
+
+    @Post('login')
+    login(){
+        this.authService.login()
+    }
+    
+}
