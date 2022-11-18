29a6f397c77644e40e23e062ca3ef20922de59cd
AliFaiizan <alifaizanahmad@gmail.com>
2022-11-18 23:04:24 +0500

    main

--- metabaseTV/src/index.js
+++ metabaseTV/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// this will the gome page of meta base
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
