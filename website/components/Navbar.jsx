876441682e421e553740668ef7c2a79994a3b557
AliFaiizan <alifaizanahmad@gmail.com>
2022-11-30 03:00:20 +0500

    navbar

--- website/components/Navbar.jsx
+++ website/components/Navbar.jsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const Navbar = () => (
-  <div>Navbar</div>
-);
+import images from '../assets';
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:w-dark bg-white dark:bg-w-black-1 w-grey-1">
+      <div className="flex flex-1 flex-row justify-between">
+        <Link href="/">
+          <div className="flexCenter md:hidden cursor-pointer " onClick={() => {}}>
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+            <p className="dark:text-white text-w-black-1 font-semibold text-lg ml-1">
+              GrandMaster
+            </p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="hidden md:flex" onClick={() => {}}>
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+          </div>
+        </Link>
+
+        <div className="flex flex-initial flex-row justify-end">
+          <div className="flex items-center mr-2">
+            <input
+              type="checkbox"
+              className="checkbox"
+              id="checkbox"
+              onChange={() => {
+                setTheme(theme === 'dark' ? 'light' : 'dark');
+              }}
+            />
+            <label htmlFor="checkbox" className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label">
+              <i className="fas fa-sun" />
+              <i className="fas fa-moon" />
+              <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+            </label>
+          </div>
+        </div>
+
+        <div>
+          MENU ITEMS
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -23,55 +23,55 @@ module.exports = {
         'file-reject': '#ff1744',
         'overlay-black': 'rgba(0, 0, 0, 0.5)',
       },
-      width:{
-        215:'215px',
-        357:'357px',
-        557:'557px',
+      width: {
+        215: '215px',
+        357: '357px',
+        557: '557px',
       },
-      minWidth:{
-        155:'155px',
-        190:'190px',
-        215:'215px',
-        240:'240px',
-        256:'256px',
-        327:'327px',
+      minWidth: {
+        155: '155px',
+        190: '190px',
+        215: '215px',
+        240: '240px',
+        256: '256px',
+        327: '327px',
       },
-      height:{
-        300:'300px',
-        557:'557px',
+      height: {
+        300: '300px',
+        557: '557px',
       },
-      inset:{
-        45:'45%',
-        65:'65px',
+      inset: {
+        45: '45%',
+        65: '65px',
       },
-      spacing:{
-        65:'65px',
+      spacing: {
+        65: '65px',
       },
-      flex:{
-        2:'2 2 0%',
+      flex: {
+        2: '2 2 0%',
       },
-      lineHeight:{
-        70:'70px',
+      lineHeight: {
+        70: '70px',
       },
-      zIndex:{
-        '-5':'-5',
-        0:'0',
-      }, 
+      zIndex: {
+        '-5': '-5',
+        0: '0',
+      },
       screens: {
         lg: { max: '1800px' },
         md: { max: '990px' },
         sm: { max: '600px' },
         xs: { max: '400px' },
-        minmd:'1700px',
-        minlg:'2100px',
+        minmd: '1700px',
+        minlg: '2100px',
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
-      }
+      },
     },
   },
   variants: {
-    extend: {}, 
+    extend: {},
   },
   plugins: [],
-}
+};
