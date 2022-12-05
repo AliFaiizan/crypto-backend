dc7edcaf54350e0b804cd6841d87b98f3a16b659
AliFaiizan <alifaizanahmad@gmail.com>
2022-12-06 02:38:36 +0500

    NFT Cards

--- website/components/index.js
+++ website/components/index.js
@@ -1,4 +1,9 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Banner from './Banner';
+import CreatorCard from './CreatorCard';
+import Button from './Button';
+import NFTCard from './NFTCard';
+import Signal from './Signal';
 
-export { Navbar, Footer };
+export { Navbar, Footer, Banner, Button, CreatorCard, NFTCard, Signal };
--- website/pages/index.js
+++ website/pages/index.js
@@ -1,8 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
-import Banner from '../components/Banner';
-import CreatorCard from '../components/CreatorCard';
+import { Banner, CreatorCard, NFTCard } from '../components';
 import images from '../assets';
 import { makeId } from '../utils/makeId';
 
@@ -38,8 +37,8 @@ const Home = () => {
   });
 
   return (
-    <div className="flex justify-center sm:px-1 p-12">
-      <div className="w-full md:w-4/5">
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
         <Banner
           name="Discover The Best Online Crypto Community"
           substyles="md:text-4xl sm:text-2xl sx:text-xl text-left"
@@ -64,30 +63,54 @@ const Home = () => {
                 />
               ))}
               {!hideButtons && (
-              <>
-                <div
-                  onClick={() => handleScroll('left')}
-                  className="absolute w-8 h-8 minlg:w-12 min:h-12 top-45 cursor-pointer left-0"
-                >
-                  <Image
-                    src={images.left}
-                    alt="lefticon"
-                    className={theme === 'light' && 'filter invert'}
-                  />
-                </div>
-                <div
-                  onClick={() => handleScroll('right')}
-                  className="absolute w-8 h-8 minlg:w-12 min:h-12 top-45 cursor-pointer right-0"
-                >
-                  <Image
-                    src={images.right}
-                    alt="righticon"
-                    className={theme === 'light' && 'filter invert'}
-                  />
-                </div>
-              </>
+                <>
+                  <div
+                    onClick={() => handleScroll('left')}
+                    className="absolute w-8 h-8 minlg:w-12 min:h-12 top-45 cursor-pointer left-0"
+                  >
+                    <Image
+                      src={images.left}
+                      alt="lefticon"
+                      className={theme === 'light' && 'filter invert'}
+                    />
+                  </div>
+                  <div
+                    onClick={() => handleScroll('right')}
+                    className="absolute w-8 h-8 minlg:w-12 min:h-12 top-45 cursor-pointer right-0"
+                  >
+                    <Image
+                      src={images.right}
+                      alt="righticon"
+                      className={theme === 'light' && 'filter invert'}
+                    />
+                  </div>
+                </>
               )}
             </div>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className=" flex-1 before:first:font-poppins text-color text-2xl minlg:text-4xl font-semibold sm:mb-4">
+              Best Nfts
+            </h1>
+            <div>Search</div>
+          </div>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-evenly">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft${i}`}
+                nft={{
+                  i,
+                  name: `GMNFT${i}`,
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  owner: `0x${makeId(3)}...${makeId(4)}`,
+                  price: 0.13 * i,
+                  description: 'Best NFT',
+                }}
+              />
+            ))}
           </div>
         </div>
       </div>
