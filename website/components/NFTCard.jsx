7f226df8871bbb253b36397e0c62a49be2a1f42c
AliFaiizan <alifaizanahmad@gmail.com>
2022-12-07 23:16:25 +0500

    signal styles + creator button fix

--- website/components/NFTCard.jsx
+++ website/components/NFTCard.jsx
@@ -6,7 +6,7 @@ import images from '../assets';
 const NFTCard = ({ nft }) => (
   <Link href={{ pathname: '/nfts/detail', query: nft }}>
     <div className=" flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-w-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md ">
-      <div className="relative w-full h-52 sm:h-36 xs:h-56 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden ">
+      <div title="NFT" className="relative w-full h-52 sm:h-36 xs:h-56 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden ">
         <Image
           src={nft.image || images[`nft${nft.i}`]}
           alt={`nft${nft.i}`}
@@ -14,14 +14,23 @@ const NFTCard = ({ nft }) => (
         />
       </div>
       <div className="mt-3 flex flex-col ">
-        <p className="font-poppins text-color font-semibold minlg:text-xl text-sm">
+        <p
+          title="Nft Name"
+          className="font-poppins text-color font-semibold minlg:text-xl text-sm"
+        >
           {nft.name}
         </p>
         <div className="flexBetween mt-1 minlg:md-3 flex-row xs:flex-col xs:items-start xs:mt-3">
-          <p className="font-poppins text-color font-semibold minlg:text-lg text-xs">
+          <p
+            title="Price"
+            className="font-poppins text-color font-semibold minlg:text-lg text-xs"
+          >
             {nft.price.toFixed(2)} <span className="normal">ETH</span>
           </p>
-          <p className="font-poppins text-color font-semibold text-xs minlg:text-lg">
+          <p
+            title="owner"
+            className="font-poppins text-color font-semibold text-xs minlg:text-lg"
+          >
             {nft.seller}
           </p>
         </div>
