dde91f6b74fb4902964942dca4d7403466df0016
AliFaiizan <alifaizanahmad@gmail.com>
2022-12-11 23:56:33 +0500

    fetch Items Listed and my nfts

--- website/contracts/Lock.sol
+++ website/contracts/Lock.sol
@@ -123,6 +123,50 @@ contract NFT is ERC721URIStorage{
         return items;
     }
 
+    function fetchMyNFTs() public view return (MarketItem[] memory){
+        uint totalItemCount=_tokenIds.current();
+        uint itemCount=0;
+        uint currentIndex=0;
+
+        for(uint i=0;i<total;i++){
+            if(idToMarketItem[i+1].owner==msg.sender){
+                itemCount+=1;
+            }
+        }
+        MarketItem[] memory items=new MarketItem[](itemCount);
+         for(uint i=0;i<totalItemCount;i++){
+            if(idToMarketItem[i+1].owner==msg.sender){
+                uint currentId=i+1;
+                MarketItem storage currentItem=idToMarketItem[currentId];
+                items[currentIndex]=currentItem;
+                currentIndex+=1;
+            }
+        }
+        return items;
+    }
+
+    function fetchItemsListed()public view returns (marketItems[] memory){
+        uint totalItemCount=_tokenIds.current();
+        uint itemCount=0;
+        uint currentIndex=0;
+
+        for(uint i=0;i<total;i++){
+            if(idToMarketItem[i+1].seller==msg.sender){
+                itemCount+=1;
+            }
+        }
+        MarketItem[] memory items=new MarketItem[](itemCount);
+         for(uint i=0;i<totalItemCount;i++){
+            if(idToMarketItem[i+1].seller==msg.sender){
+                uint currentId=i+1;
+                MarketItem storage currentItem=idToMarketItem[currentId];
+                items[currentIndex]=currentItem;
+                currentIndex+=1;
+            }
+        }
+        return items;
+    }
+
     function awardItem(address player, string memory tokenURI)
         public
         returns (uint256)
